64ecba59f1b5944d32064477e1dc5f2aa97bab66
witheredwood <906185091@qq.com>
2021-11-26 22:03:22 +0800

    283

--- test/code/S02_array/code_0283Test.java
+++ test/code/S02_array/code_0283Test.java
@@ -1,5 +1,26 @@
+package code.S02_array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class code_0283Test {
 
+/**
+ * 示例:
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ */
+class code_0283Test extends code_0283 {
+    int[] nums = {0, 1, 0, 3, 12};
+    int[] expected = {1, 3, 12, 0, 0};
+
+    @Test
+    void testMoveZeroes() {
+        System.out.println("nums = " + Arrays.toString(nums));
+        moveZeroes(nums);
+        Assertions.assertArrayEquals(expected, nums);
+    }
 }
