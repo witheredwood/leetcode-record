05bfaf54de41ff2365fa3664434c2a29f33bd05b
witheredwood <906185091@qq.com>
2021-11-28 18:08:43 +0800

    209

---
+++ src/code/S02_array/code_0209.java
@@ -0,0 +1,51 @@
+package code.S02_array;
+
+/**
+ * 209. 长度最小的子数组
+ * <p>
+ * 给定一个含有 n 个正整数的数组和一个正整数 target 。
+ * 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
+ * <p>
+ * 示例 1：
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+ * 示例 2：
+ * 输入：target = 4, nums = [1,4,4]
+ * 输出：1
+ * 示例 3：
+ * 输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+ * 输出：0
+ *  
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-size-subarray-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class code_0209 {
+    /**
+     * @param target 目标和，正整数
+     * @param nums   正整数数组
+     * @return 子数组之和不小于 target 的数组长度
+     */
+    public int minSubArrayLen(int target, int[] nums) {
+        int len = nums.length;
+        int res = 0, sum = 0;
+        // 找到从0开始的子数组
+        for (int i = 0; i < len && sum < target; i++) {
+            sum += nums[i];
+            res = i + 1;
+        }
+        System.out.println("res = " + res + " sum = " + sum);
+        if (sum < target) return 0;  // 所有数加起来也小于 target
+        sum -= nums[res - 1];
+        for (int left = 1, right = res - 1; left <= right && right < len; left++, right++) {
+            sum = sum - nums[left - 1] + nums[right];
+            while (left <= right && sum >= target) {
+                res = Math.min(res, right - left + 1);
+                sum -= nums[left++];
+            }
+        }
+        return res;
+    }
+}
---
+++ test/code/S02_array/code_0209Test.java
@@ -0,0 +1,63 @@
+package code.S02_array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * 示例 1：
+ * 输入：target = 7, nums = [2,3,1,2,4,3]
+ * 输出：2
+ * 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+ * 示例 2：
+ * 输入：target = 4, nums = [1,4,4]
+ * 输出：1
+ * 示例 3：
+ * 输入：target = 11, nums = [1,1,1,1,1,1,1,1]
+ * 输出：0
+ */
+class code_0209Test extends code_0209 {
+    int target1 = 7;
+    //    int[] nums1 = {4, 3, 2, 3, 1, 2};
+    int[] nums1 = {2, 3, 1, 2, 4, 3};
+    int expected1 = 2;
+
+    int target2 = 4;
+    int[] nums2 = {1, 4, 4};
+    int expected2 = 1;
+
+    int target3 = 11;
+    int[] nums3 = {1, 1, 1, 1, 1, 1, 1, 1};
+    int expected3 = 0;
+
+    int target4 = 11;
+    int[] nums4 = {1, 2, 3, 4, 5};
+    int expected4 = 3;
+
+    @Test
+    void testMinSubArrayLen1() {
+        System.out.println("nums = " + Arrays.toString(nums1) + " target = " + target1);
+        Assertions.assertEquals(expected1, minSubArrayLen(target1, nums1));
+    }
+
+    @Test
+    void testMinSubArrayLen2() {
+        System.out.println("nums = " + Arrays.toString(nums2) + " target = " + target2);
+        Assertions.assertEquals(expected2, minSubArrayLen(target2, nums2));
+    }
+
+    @Test
+    void testMinSubArrayLen3() {
+        System.out.println("nums = " + Arrays.toString(nums3) + " target = " + target3);
+        Assertions.assertEquals(expected3, minSubArrayLen(target3, nums3));
+    }
+
+    @Test
+    void testMinSubArrayLen4() {
+        System.out.println("nums = " + Arrays.toString(nums4) + " target = " + target4);
+        Assertions.assertEquals(expected4, minSubArrayLen(target4, nums4));
+    }
+}
