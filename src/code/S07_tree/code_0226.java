21176f4217a9c35b2e66ddf6a71ed3d813f4d003
witheredwood <906185091@qq.com>
2022-01-14 20:49:25 +0800

    226

--- src/code/S07_tree/code_0226.java
+++ src/code/S07_tree/code_0226.java
@@ -9,23 +9,27 @@ import java.util.LinkedList;
  *
  * 示例：
  * 输入：
- * 
- * 
- * 7
- * \
- * 1 6 9
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
  * 输出：
- * 
- * 
- * 2
- * \
- * 9 3 1
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
  *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/invert-binary-tree
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class code_0226 {
+    /**
+     * @param root 二叉树根节点
+     * @return 翻转后的二叉树根节点
+     */
     public TreeNode invertTree(TreeNode root) {
         LinkedList<TreeNode> queue = new LinkedList<>();
         if (root != null)
