75669ec4f3b95dc1830026b34aae1b5b950f85ed
witheredwood <witheredwood7@163.com>
2022-07-17 12:14:18 +0800

    941

--- src/code/S02_array/code_0941.java
+++ src/code/S02_array/code_0941.java
@@ -24,22 +24,40 @@ package code.S02_array;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class code_0941 {
-    /**
-     * @param arr 给定的整数数组
-     * @return true-该数组是先增后减数组；false-该数组不是先增后减数组
-     */
-    public boolean validMountainArray(int[] arr) {
-        int len = arr.length;
-        if (len < 3) return false;
-        int index = 1;
-        while (index < len && arr[index] > arr[index - 1]) {
-            index++;
-        }
-        // 单调递增或单调递减数组都不是山脉
-        if (index == 1 || index >= len) return false;
-        while (index < len && arr[index] < arr[index - 1]) {
-            index++;
-        }
-        return index >= len;
+  /**
+   * 山脉数组，只有1个单调递增子数组和1个单调递减子数组
+   * time: O(n);    space: O(1)
+   *
+   * @param arr 给定的整数数组
+   * @return true-该数组是先增后减数组；false-该数组不是先增后减数组
+   */
+  public boolean validMountainArray31(int[] arr) {
+    if (arr.length < 3) return false;
+    int index = 1;
+    // 找到上升子数组的右端点
+    while (index < arr.length && arr[index] > arr[index - 1]) index++;
+    if (index == 1 || index == arr.length) return false;  // 没有上升子数组或者只有上升子数组，均返回错误
+    // 找到下降子数组的右端点
+    while (index < arr.length && arr[index] < arr[index - 1]) index++;
+    return index == arr.length;
+  }
+
+  /**
+   * @param arr 给定的整数数组
+   * @return true-该数组是先增后减数组；false-该数组不是先增后减数组
+   */
+  public boolean validMountainArray(int[] arr) {
+    int len = arr.length;
+    if (len < 3) return false;
+    int index = 1;
+    while (index < len && arr[index] > arr[index - 1]) {
+      index++;
     }
+    // 单调递增或单调递减数组都不是山脉
+    if (index == 1 || index >= len) return false;
+    while (index < len && arr[index] < arr[index - 1]) {
+      index++;
+    }
+    return index >= len;
+  }
 }
