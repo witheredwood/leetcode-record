733354b9361a78f1b3d00d861b47996e05be7088
witheredwood <witheredwood7@163.com>
2022-07-17 12:48:38 +0800

    704

--- src/code/S02_array/code_0704.java
+++ src/code/S02_array/code_0704.java
@@ -3,13 +3,14 @@ package code.S02_array;
 /**
  * 704. 二分查找
  * <p>
- * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ * 给定一个n个元素有序的（升序）整型数组nums 和一个目标值target ，写一个函数搜索nums中的 target，
+ * 如果目标值存在返回下标，否则返回 -1。
  * <p>
  * 示例 1:
  * 输入: nums = [-1,0,3,5,9,12], target = 9
  * 输出: 4
  * 解释: 9 出现在 nums 中并且下标为 4
- * 示例 2:
+ * 示例2:
  * 输入: nums = [-1,0,3,5,9,12], target = 2
  * 输出: -1
  * 解释: 2 不存在 nums 中因此返回 -1
@@ -19,19 +20,38 @@ package code.S02_array;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class code_0704 {
-    /**
-     * @param nums   数组
-     * @param target 目标值
-     * @return 目标值在数组中的下标
-     */
-    public int search(int[] nums, int target) {
-        int left = 0, right = nums.length - 1, mid;
-        while (left <= right) {
-            mid = (left + right) / 2;
-            if (nums[mid] == target) return mid;
-            if (nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
-        }
-        return -1;
+  /**
+   * @param nums   数组
+   * @param target 目标值
+   * @return 目标值在数组中的下标
+   */
+  public int search31(int[] nums, int target) {
+    int low = 0, high = nums.length, mid;
+    while (low < high) {
+      mid = (low + high) / 2;
+      if (target == nums[mid]) return mid;
+      if (target < nums[mid]) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
     }
+    return -1;
+  }
+
+  /**
+   * @param nums   数组
+   * @param target 目标值
+   * @return 目标值在数组中的下标
+   */
+  public int search(int[] nums, int target) {
+    int left = 0, right = nums.length - 1, mid;
+    while (left <= right) {
+      mid = (left + right) / 2;
+      if (nums[mid] == target) return mid;
+      if (nums[mid] < target) left = mid + 1;
+      else right = mid - 1;
+    }
+    return -1;
+  }
 }
