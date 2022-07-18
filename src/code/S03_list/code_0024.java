1a866c81753bf8aa52142f46ac166fd44f0a4e58
witheredwood <witheredwood7@163.com>
2022-07-18 12:58:02 +0800

    24

--- src/code/S03_list/code_0024.java
+++ src/code/S03_list/code_0024.java
@@ -24,55 +24,85 @@ package code.S03_list;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class code_0024 {
-    /**
-     * @param head 头结点
-     * @return 交换后的链表头结点
-     */
-    public ListNode swapPairs(ListNode head) {
-        ListNode dummy = new ListNode(); // 虚节点
-        dummy.next = head;
-        ListNode first = head, second, tmp = null;
-        ListNode p = dummy;
-        while (first != null) {
-            second = first.next;
-            if (second != null) {
-                tmp = second.next;
-                p.next = second;
-                p = p.next;
-            } else {
-                tmp = null;
-            }
-            p.next = first;
-            p = p.next;
-            p.next = null;
-            first = tmp;
-        }
-        System.out.println("duumy = " + dummy.next.val);
-        return dummy.next;
+  /**
+   * time: O(n);    space: O(1)
+   *
+   * @param head 头结点
+   * @return 交换后的链表头结点
+   */
+  public ListNode swapPairs31(ListNode head) {
+    // 加入虚节点
+    ListNode dummy = new ListNode(0, head);
+    head = dummy;
+    // 遍历变量
+    ListNode last = head;  // 已排过序的链表的最后一个节点
+    ListNode p1 = head.next, p2 = null;  // 要交换的相邻的节点
+    ListNode start;  // 未交换的链表的第1个节点
+    while (p1 != null && p1.next != null) {
+      p2 = p1.next;
+      // 交换相邻节点
+      start = p2.next;
+      p2.next = p1;
+      p1.next = start;
+      // 加入已排过序的链表中
+      last.next = p2;
+      // 更新遍历变量
+      last = last.next.next;
+      p1 = start;
     }
+    last.next = p1;
+    return head.next;
+  }
 
-    /**
-     * @param head 头结点
-     * @return 交换后的链表头结点
-     */
-    public ListNode swapPairs2(ListNode head) {
-        ListNode dummy = new ListNode(); // 虚节点
-        dummy.next = head;
-        ListNode pre = dummy, first = head, second, tmp;
-        while (first != null && first.next != null) {
-            // 变量
-            second = first.next;
-            tmp = second.next;
-            // 交换
-            pre.next = second;
-            second.next = first;
-            first.next = tmp;
-            // 移动引用
-            first = tmp;
-            pre = pre.next.next;
-        }
-        pre.next = first;
-        System.out.println("duumy = " + dummy.next.val);
-        return dummy.next;
+  /**
+   * @param head 头结点
+   * @return 交换后的链表头结点
+   */
+  public ListNode swapPairs(ListNode head) {
+    ListNode dummy = new ListNode(); // 虚节点
+    dummy.next = head;
+    ListNode first = head, second, tmp = null;
+    ListNode p = dummy;
+    while (first != null) {
+      second = first.next;
+      if (second != null) {
+        tmp = second.next;
+        p.next = second;
+        p = p.next;
+      } else {
+        tmp = null;
+      }
+      p.next = first;
+      p = p.next;
+      p.next = null;
+      first = tmp;
     }
+    System.out.println("duumy = " + dummy.next.val);
+    return dummy.next;
+  }
+
+  /**
+   * @param head 头结点
+   * @return 交换后的链表头结点
+   */
+  public ListNode swapPairs2(ListNode head) {
+    ListNode dummy = new ListNode(); // 虚节点
+    dummy.next = head;
+    ListNode pre = dummy, first = head, second, tmp;
+    while (first != null && first.next != null) {
+      // 变量
+      second = first.next;
+      tmp = second.next;
+      // 交换
+      pre.next = second;
+      second.next = first;
+      first.next = tmp;
+      // 移动引用
+      first = tmp;
+      pre = pre.next.next;
+    }
+    pre.next = first;
+    System.out.println("duumy = " + dummy.next.val);
+    return dummy.next;
+  }
 }
