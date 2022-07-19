6c166d1a23499037573b9a8fe7f746257203f476
witheredwood <witheredwood7@163.com>
2022-07-19 11:43:47 +0800

    142

--- src/code/S03_list/code_0142.java
+++ src/code/S03_list/code_0142.java
@@ -1,10 +1,13 @@
 package code.S03_list;
 
+
 /**
  * 142.环形链表II
  * <p>
- * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
- * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ * 给定一个链表，返回链表开始入环的第一个节点。如果链表无环，则返回null。
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。
+ * 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
  * 不允许修改 链表。
  * <p>
  * 示例 1：
@@ -30,33 +33,72 @@ package code.S03_list;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class code_0142 {
-    /**
-     * 慢指针每次走一步，快指针每次走一步。如果有环，两个指针一定会在环中相遇。（快指针相对于慢指针，是每次走一步）
-     * 从头结点出发一个指针，从相遇节点 也出发一个指针，这两个指针每次只走一个节点， 那么当这两个指针相遇的时候就是 环形入口的节点。
-     * @param head 链表的头结点
-     * @return 链表中有环，则返回进入环的第一个节点。链表中无环，则返回 null
-     */
-    public ListNode detectCycle(ListNode head) {
-        ListNode dummy = new ListNode(0, head);  // 虚节点
-        ListNode slow = dummy.next, fast = dummy.next;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-            if (slow == fast){  // 第一次相遇
-                // 寻找入口
-                System.out.println("slow = " + slow.val + " fast = " + fast.val);
-                ListNode p1 = head, p2 = fast;
-                while (p1 != p2) {
-                    p1 = p1.next;
-                    p2 = p2.next;
-                }
-                return p1;
-            }
-        }
-        // 寻找入口
-        if (slow == fast) {
+  /**
+   * 慢指针每次走一步，快指针每次走一步。如果有环，两个指针一定会在环中相遇。（快指针相对于慢指针，是每次走一步）
+   * 从头结点出发一个指针，从相遇节点 也出发一个指针，这两个指针每次只走一个节点， 那么当这两个指针相遇的时候就是 环形入口的节点。
+   */
 
-        }
-        return null;
+  /**
+   * 慢指针和快指针不在同一起点
+   * time: O(n);   space: O(1)
+   * 慢指针走的总路程  s = z + y
+   * 快指针走的总路程 f = z - 1 + (x + y) + y = z + x + 2y
+   * 由 2s = f 得： z = x - 1
+   *
+   * @param head 链表的头结点
+   * @return 链表中有环，则返回进入环的第一个节点。链表中无环，则返回 null
+   */
+  public ListNode detectCycle32(ListNode head) {
+    // 加入虚节点，让空链表的操作和其他一样
+    ListNode dummy = new ListNode(0, head);
+    head = dummy;
+    // 找慢指针和快指针相遇的地方
+    ListNode slow = head, fast = head.next;
+    while (fast != null && fast.next != null && fast.next.next != null && fast != slow) {
+      fast = fast.next.next;
+      slow = slow.next;
     }
+    // 无环
+    if (fast != slow) return null;
+    // 有环
+    slow = head;
+    fast = fast.next;  // 快指针先走 1 步
+    while (fast != slow) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+    return slow;
+  }
+
+  /**
+   * 慢指针和快指针在同一起点
+   * time: O(n);   space: O(1)
+   * 慢指针走的总路程  s = z + y
+   * 快指针走的总路程 f = z + (x + y) + y = z + x + 2y
+   * 由 2s = f 得： z = x
+   *
+   * @param head 链表的头结点
+   * @return 链表中有环，则返回进入环的第一个节点。链表中无环，则返回 null
+   */
+  public ListNode detectCycle31(ListNode head) {
+    // 加入虚节点，让空链表的操作和其他一样
+    ListNode dummy = new ListNode(0, head);
+    head = dummy;
+    // 找慢指针和快指针相遇的地方
+    ListNode slow = head, fast = head;
+    while (fast.next != null && fast.next.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+      if (fast == slow) break;
+    }
+    // 无环
+    if (fast != slow) return null;
+    // 有环
+    slow = head;
+    while (fast != slow) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+    return slow;
+  }
 }
