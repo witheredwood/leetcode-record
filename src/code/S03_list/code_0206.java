2e0a081ff04da33985818c6ae573e97d25c0b9e8
witheredwood <witheredwood7@163.com>
2022-07-20 15:14:30 +0800

    206

--- src/code/S03_list/code_0206.java
+++ src/code/S03_list/code_0206.java
@@ -22,39 +22,55 @@ import java.util.LinkedList;
  * -5000 <= Node.val <= 5000
  */
 public class code_0206 {
-    /**
-     * @param head 头节点
-     * @return 反转后的链表的头结点
-     */
-    public ListNode reverseList(ListNode head) {
-        if (head == null) return head;
-        ListNode yummy = new ListNode();  // 虚节点
-        yummy.next = head;
-        ListNode p = head.next, q;  // 引用
-        head.next = null;
-        while (p != null) {
-            q = p.next;  // 保存
-            p.next = yummy.next;
-            yummy.next = p;
-            p = q;
-        }
-        System.out.println("head = " + yummy.next.val);
-        return yummy.next;
+  /**
+   * @param head 链表的头节点
+   * @return 反转后的链表的头结点
+   */
+  public ListNode reverseList31(ListNode head) {
+    ListNode dummy = new ListNode(0, null);
+    ListNode cur = head, newHead = null;
+    while (cur != null) {
+       newHead = cur.next;
+      cur.next = dummy.next;
+      dummy.next = cur;
+      cur = newHead;
     }
+    return dummy.next;
+  }
 
-    /**
-     * @param head 头节点
-     * @return 反转后的链表的头结点
-     */
-    public ListNode reverseList2(ListNode head) {
-        ListNode pre = null, cur = head, tmp;  // 引用
-        while (cur != null) {
-            tmp = cur.next;  // 暂时保存cur的下一个节点
-            cur.next = pre;  // 保存
-            pre = cur;
-            cur = tmp;
-        }
-        System.out.println("head = " + pre.val);
-        return pre;
+  /**
+   * @param head 头节点
+   * @return 反转后的链表的头结点
+   */
+  public ListNode reverseList(ListNode head) {
+    if (head == null) return head;
+    ListNode yummy = new ListNode();  // 虚节点
+    yummy.next = head;
+    ListNode p = head.next, q;  // 引用
+    head.next = null;
+    while (p != null) {
+      q = p.next;  // 保存
+      p.next = yummy.next;
+      yummy.next = p;
+      p = q;
     }
+    System.out.println("head = " + yummy.next.val);
+    return yummy.next;
+  }
+
+  /**
+   * @param head 头节点
+   * @return 反转后的链表的头结点
+   */
+  public ListNode reverseList2(ListNode head) {
+    ListNode pre = null, cur = head, tmp;  // 引用
+    while (cur != null) {
+      tmp = cur.next;  // 暂时保存cur的下一个节点
+      cur.next = pre;  // 保存
+      pre = cur;
+      cur = tmp;
+    }
+    System.out.println("head = " + pre.val);
+    return pre;
+  }
 }
