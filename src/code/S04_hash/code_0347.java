6a795f60a77dbd3df367434a183ad457062cf048
witheredwood <witheredwood7@163.com>
2022-07-24 10:29:53 +0800

    347

--- src/code/S04_hash/code_0347.java
+++ src/code/S04_hash/code_0347.java
@@ -19,65 +19,96 @@ import java.util.*;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class code_0347 {
-	/**
-	 * 优先级队列
-	 *
-	 * @param nums 给定的正整数数组
-	 * @param k    给定的返回前几个高频数
-	 * @return 前 k 个高频数的数组
-	 */
-	public int[] topKFrequent(int[] nums, int k) {
+
+  /**
+   * time: O(nlogn);			space: O(n)
+   *
+   * @param nums 给定的正整数数组
+   * @param k    给定的返回前几个高频数
+   * @return 前 k 个高频数的数组
+   */
+  public int[] topKFrequent31(int[] nums, int k) {
+		// 统计各元素的个数
 		Map<Integer, Integer> map = new HashMap<>();
-		// 计数
-		for (int val : nums) {
-			if (map.containsKey(val)) map.replace(val, map.get(val) + 1);
-			else map.put(val, 1);
+		for (int i = 0; i < nums.length; i++) {
+			if (!map.containsKey(nums[i])) {
+				map.put(nums[i], 1);
+			} else {
+				map.replace(nums[i], map.get(nums[i]) + 1);
+			}
 		}
-		// 使用优先级队列排序
+		// 排序。使用 优先级队列 升序排列
 		PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> {
-			return map.get(a) - map.get(b);
+			if (map.get(a) != map.get(b)) return map.get(a) - map.get(b);
+			return a - b;
 		});
-		for (int key : map.keySet()) {
-			queue.offer(key);
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			queue.offer(entry.getKey());
 			if (queue.size() > k) queue.poll();
 		}
-		int[] res = new int[k];
-		int i = 0;
-		while (!queue.isEmpty()) {
-			res[i++] = queue.poll();
-		}
-		return res;
-	}
+		Integer[] result = queue.toArray(new Integer[0]);
+		return Arrays.stream(result).mapToInt(Integer::valueOf).toArray();
+  }
 
-	/**
-	 * 计数+排序+哈希
-	 *
-	 * @param nums 给定的正整数数组
-	 * @param k    给定的返回前几个高频数
-	 * @return 前 k 个高频数的数组
-	 */
-	public int[] topKFrequent1(int[] nums, int k) {
-		Map<Integer, Integer> map = new TreeMap<>();
-		// 计数
-		for (int val : nums) {
-			if (map.containsKey(val))
-				map.replace(val, map.get(val) + 1);
-			else
-				map.put(val, 1);
-		}
-		List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>(map.entrySet());
-		Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-			@Override
-			public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-				return o2.getValue() - o1.getValue(); // 降序
-			}
-		});
-		// 返回结果
-		int[] res = new int[k];
-		for (int i = 0; i < k; i++) {
-			res[i] = list.get(i).getKey();
-		}
-		System.out.println("res = " + Arrays.toString(res));
-		return res;
-	}
+  /**
+   * 优先级队列
+   *
+   * @param nums 给定的正整数数组
+   * @param k    给定的返回前几个高频数
+   * @return 前 k 个高频数的数组
+   */
+  public int[] topKFrequent(int[] nums, int k) {
+    Map<Integer, Integer> map = new HashMap<>();
+    // 计数
+    for (int val : nums) {
+      if (map.containsKey(val)) map.replace(val, map.get(val) + 1);
+      else map.put(val, 1);
+    }
+    // 使用优先级队列排序
+    PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> {
+      return map.get(a) - map.get(b);
+    });
+    for (int key : map.keySet()) {
+      queue.offer(key);
+      if (queue.size() > k) queue.poll();
+    }
+    int[] res = new int[k];
+    int i = 0;
+    while (!queue.isEmpty()) {
+      res[i++] = queue.poll();
+    }
+    return res;
+  }
+
+  /**
+   * 计数+排序+哈希
+   *
+   * @param nums 给定的正整数数组
+   * @param k    给定的返回前几个高频数
+   * @return 前 k 个高频数的数组
+   */
+  public int[] topKFrequent1(int[] nums, int k) {
+    Map<Integer, Integer> map = new TreeMap<>();
+    // 计数
+    for (int val : nums) {
+      if (map.containsKey(val))
+        map.replace(val, map.get(val) + 1);
+      else
+        map.put(val, 1);
+    }
+    List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>(map.entrySet());
+    Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+      @Override
+      public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+        return o2.getValue() - o1.getValue(); // 降序
+      }
+    });
+    // 返回结果
+    int[] res = new int[k];
+    for (int i = 0; i < k; i++) {
+      res[i] = list.get(i).getKey();
+    }
+    System.out.println("res = " + Arrays.toString(res));
+    return res;
+  }
 }
