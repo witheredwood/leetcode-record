a795fd3adb7f86fa20739a16077b2260884e235b
witheredwood <witheredwood7@163.com>
2022-07-24 15:19:19 +0800

    705

---
+++ src/code/S04_hash/code_0705.java
@@ -0,0 +1,81 @@
+package code.S04_hash;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * 705. 设计哈希集合
+ * <p>
+ * 不使用任何内建的哈希表库设计一个哈希集合（HashSet）。
+ * 实现 MyHashSet 类：
+ * void add(key) 向哈希集合中插入值 key 。
+ * bool contains(key) 返回哈希集合中是否存在这个值 key 。
+ * void remove(key) 将给定值 key 从哈希集合中删除。如果哈希集合中没有这个值，什么也不做。
+ * <p>
+ * 示例：
+ * 输入：
+ * ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+ * [[], [1], [2], [1], [3], [2], [2], [2], [2]]
+ * 输出：
+ * [null, null, null, true, false, null, true, null, false]
+ * 解释：
+ * MyHashSet myHashSet = new MyHashSet();
+ * myHashSet.add(1);      // set = [1]
+ * myHashSet.add(2);      // set = [1, 2]
+ * myHashSet.contains(1); // 返回 True
+ * myHashSet.contains(3); // 返回 False ，（未找到）
+ * myHashSet.add(2);      // set = [1, 2]
+ * myHashSet.contains(2); // 返回 True
+ * myHashSet.remove(2);   // set = [1]
+ * myHashSet.contains(2); // 返回 False ，（已移除）
+ * <p>
+ * 提示：
+ * 0 <= key <= 106
+ * 最多调用 104 次 add、remove 和 contains
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/design-hashset
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class code_0705 {
+  private int base;
+  private List<Integer>[] array = null;
+
+  code_0705() {
+    this.base = 727;
+    this.array = new ArrayList[base];
+  }
+
+  code_0705(int base) {
+    this.base = base;
+    this.array = new List[base];
+  }
+
+  public void add(int key) {
+    int index = key % this.base;
+    if (this.array[index] == null) {
+      this.array[index] = new ArrayList<>();
+    }
+    this.array[index].add(key);
+  }
+
+  public void remove(int key) {
+    int index = key % this.base;
+    if (this.array[index] == null) return;
+    Iterator<Integer> it = this.array[index].iterator();
+    while (it.hasNext()) {
+      if (key == it.next()) it.remove();
+    }
+  }
+
+  public boolean contains(int key) {
+    int index = key % this.base;
+    if (this.array[index] == null) return false;
+    for (int i = 0; i < this.array[index].size(); i++) {
+      if (key == this.array[index].get(i))
+        return true;
+    }
+    return false;
+  }
+}
