9ed0ffcead4de96c6be8f832220847745d9a82aa
witheredwood <witheredwood7@163.com>
2022-07-25 19:25:18 +0800

    767

--- src/code/S05_string/code_0767.java
+++ src/code/S05_string/code_0767.java
@@ -1,44 +1,67 @@
 package code.S05_string;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+/**
+ * 767. 重构字符串
+ * <p>
+ * 给定一个字符串s，检查是否能重新排布其中的字母，使得两相邻的字符不同。
+ * 返回 s的任意可能的重新排列。若不可行，返回空字符串""。
+ * <p>
+ * 示例1:
+ * 输入: s = "aab"
+ * 输出: "aba"
+ * 示例 2:
+ * 输入: s = "aaab"
+ * 输出: ""
+ * <p>
+ * 提示:
+ * 1 <= s.length <= 500
+ * s 只包含小写字母
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/reorganize-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class code_0767 {
-    public static void main(String[] args) {
-        // 76 优先级队列
-        int[] num = {0, 3, 5};
-
-        PriorityQueue<Character> queue = new PriorityQueue<Character>(new Comparator<Character>() {
-            @Override
-            public int compare(Character a, Character b) {
-                return num[a - 'a'] - num[b - 'a'];
-            }
-        });
-        queue.add('b');
-        queue.add('a');
-        queue.add('c');
-        System.out.println("num = " + Arrays.toString(num) + " queue = " + queue);
-//        System.out.println("num = " + Arrays.toString(num) + " queue = " + queue.poll().toString());
-        num[0] = 9;
-        queue.poll();
-        queue.add((char) ('a' + 1));
-        System.out.println("num = " + Arrays.toString(num) + " queue = " + queue);
-        System.out.println("------------------------");
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('b', 4);
-        map.put('a', 6);
-        map.put('c', 1);
-        System.out.println("map = " + map.toString());
-        for (Character c : map.keySet()) {
-            System.out.println(c);
-        }
-
-        Map<Character, Integer> map1 = new LinkedHashMap<>();
-        map1.put('b', 4);
-        map1.put('a', 6);
-        map1.put('c', 1);
-        System.out.println("map = " + map1.toString());
-        for (Character c : map1.keySet()) {
-            System.out.println(c);
-        }
+  /**
+   * time: O(nlogn);        space: O(n)
+   *
+   * @param s 字符串
+   * @return 重排字符串
+   */
+  public String reorganizeString(String s) {
+    // 统计每个字符的个数
+    int limit = (s.length() + 1) / 2;
+    Map<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < s.length(); i++) {
+      if (!map.containsKey(s.charAt(i))) {
+        map.put(s.charAt(i), 1);
+      } else {
+        map.replace(s.charAt(i), map.get(s.charAt(i)) + 1);
+        if (map.get(s.charAt(i)) > limit) return "";
+      }
     }
+    // 排序，降序排列
+    List<Map.Entry<Character, Integer>> entryList = new ArrayList<>(map.entrySet());
+    Collections.sort(entryList, (a, b) -> {
+      if (a.getValue() != b.getValue()) return b.getValue() - a.getValue();
+      return a.getKey() - b.getKey();
+    });
+    // 重排。先填满偶数位，再填满奇数位
+    char[] resultChars = new char[s.length()];
+    int index = 0;
+    for (Map.Entry<Character, Integer> entry : entryList) {
+      for (int i = 0; i < entry.getValue(); i++) {
+        resultChars[index] = entry.getKey();
+        index += 2;
+        if (index >= s.length()) index = 1;
+      }
+    }
+    return new String(resultChars);
+  }
 }
