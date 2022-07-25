e6a254402225192c124c5332545a2e25ad1546a8
witheredwood <witheredwood7@163.com>
2022-07-25 20:32:35 +0800

    20

--- src/code/S06_stack_queue/code_0020.java
+++ src/code/S06_stack_queue/code_0020.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 /**
  * 20. 有效的括号
  * <p>
- * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
  * <p>
  * 有效字符串需满足：
  * 左括号必须用相同类型的右括号闭合。
@@ -14,21 +14,21 @@ import java.util.LinkedList;
  * 示例 1：
  * 输入：s = "()"
  * 输出：true
- * 示例 2：
+ * 示例2：
  * 输入：s = "()[]{}"
  * 输出：true
- * 示例 3：
+ * 示例3：
  * 输入：s = "(]"
  * 输出：false
- * 示例 4：
+ * 示例4：
  * 输入：s = "([)]"
  * 输出：false
- * 示例 5：
+ * 示例5：
  * 输入：s = "{[]}"
  * 输出：true
  * <p>
  * 提示：
- * 1 <= s.length <= 104
+ * 1 <= s.length <= 10^4
  * s 仅由括号 '()[]{}' 组成
  * <p>
  * 来源：力扣（LeetCode）
@@ -36,6 +36,38 @@ import java.util.LinkedList;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class code_0020 {
+    /**
+     * time: O(n);          space: O(n)
+     *
+     * @param s 字符串
+     * @return 规则之内，子字符串是否有效
+     */
+    public boolean isValid31(String s) {
+        LinkedList<Character> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case '[':
+                case '(':
+                case '{':
+                    stack.push(s.charAt(i));
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.peek() != '[') return false;
+                    stack.pop();
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.peek() != '(') return false;
+                    stack.pop();
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.peek() != '{') return false;
+                    stack.pop();
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+
     public boolean isValid(String s) {
         LinkedList<Character> stack = new LinkedList<>();
         for (char c : s.toCharArray()) {
