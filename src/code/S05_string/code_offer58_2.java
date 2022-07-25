95b612b245abc08027fe1686d951002b10ee5f00
witheredwood <witheredwood7@163.com>
2022-07-25 20:13:37 +0800

    offer58_II

--- src/code/S05_string/code_offer58_2.java
+++ src/code/S05_string/code_offer58_2.java
@@ -1,11 +1,32 @@
 package code.S05_string;
 
-public class code_offer58_1 {
+/**
+ * 剑指 Offer 58 - II. 左旋转字符串
+ * <p>
+ * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。
+ * 比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+ * <p>
+ * 示例 1：
+ * 输入: s = "abcdefg", k = 2
+ * 输出: "cdefgab"
+ * 示例 2：
+ * 输入: s = "lrloseumgh", k = 6
+ * 输出: "umghlrlose"
+ * <p>
+ * 限制：
+ * 1 <= k < s.length <= 10000
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class code_offer58_2 {
   /**
    * @param s 字符串
-   * @return
+   * @param n 整数
+   * @return 左旋转 n 位得到的结果
    */
-  public String reverseWords(String s) {
-
+  public String reverseLeftWords(String s, int n) {
+    return s.substring(n, s.length()) + s.substring(0, n);
   }
 }
