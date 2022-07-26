8c9bcf2833fb15e0e5e55044cc23e870c2eeb7fb
witheredwood <witheredwood7@163.com>
2022-07-26 14:40:29 +0800

    496

---
+++ src/code/S06_stack_queue/code_0496.java
@@ -0,0 +1,97 @@
+package code.S06_stack_queue;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * 496. 下一个更大元素 I
+ * <p>
+ * nums1中数字x的 下一个更大元素 是指x在nums2 中对应位置 右侧 的 第一个 比x大的元素。
+ * 给你两个 没有重复元素 的数组nums1 和nums2 ，下标从 0 开始计数，其中nums1是nums2的子集。
+ * 对于每个 0 <= i < nums1.length ，找出满足 nums1[i] == nums2[j] 的下标 j ，并且在 nums2 确定 nums2[j] 的 下一个更大元素 。
+ * 如果不存在下一个更大元素，那么本次查询的答案是 -1 。
+ * <p>
+ * 返回一个长度为nums1.length 的数组 ans 作为答案，满足 ans[i] 是如上所述的 下一个更大元素 。
+ * <p>
+ * 示例 1：
+ * 输入：nums1 = [4,1,2], nums2 = [1,3,4,2].
+ * 输出：[-1,3,-1]
+ * 解释：nums1 中每个值的下一个更大元素如下所述：
+ * - 4 ，用加粗斜体标识，nums2 = [1,3,4,2]。不存在下一个更大元素，所以答案是 -1 。
+ * - 1 ，用加粗斜体标识，nums2 = [1,3,4,2]。下一个更大元素是 3 。
+ * - 2 ，用加粗斜体标识，nums2 = [1,3,4,2]。不存在下一个更大元素，所以答案是 -1 。
+ * 示例 2：
+ * 输入：nums1 = [2,4], nums2 = [1,2,3,4].
+ * 输出：[3,-1]
+ * 解释：nums1 中每个值的下一个更大元素如下所述：
+ * - 2 ，用加粗斜体标识，nums2 = [1,2,3,4]。下一个更大元素是 3 。
+ * - 4 ，用加粗斜体标识，nums2 = [1,2,3,4]。不存在下一个更大元素，所以答案是 -1 。
+ * <p>
+ * 提示：
+ * 1 <= nums1.length <= nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 10^4
+ * nums1和nums2中所有整数 互不相同
+ * nums1 中的所有整数同样出现在 nums2 中
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/next-greater-element-i
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class code_0496 {
+    /**
+     * 空间换时间
+     * time: O(n+m);        space: O(n+m)
+     *
+     * @param nums1 数组
+     * @param nums2 数组
+     * @return 下一个更大元素
+     */
+    public int[] nextGreaterElement32(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+        // 初始化
+        for (int i = 0; i < result.length; i++) {
+            result[i] = -1;
+        }
+        // nums1 用 hashmap 存储
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], i);
+        }
+        // 单调栈
+        LinkedList<Integer> stack = new LinkedList<>();
+        stack.push(nums2[0]);
+        for (int i = 0; i < nums2.length; i++) {
+            while (!stack.isEmpty() && nums2[i] > stack.peek()) {
+                int key = stack.pop();
+                if (map.containsKey(key)) {
+                    result[map.get(key)] = nums2[i];
+                }
+            }
+            stack.push(nums2[i]);
+        }
+        return result;
+    }
+
+    /**
+     * time: O(nm);        space: O(n)
+     *
+     * @param nums1 数组
+     * @param nums2 数组
+     * @return 下一个更大元素
+     */
+    public int[] nextGreaterElement31(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            int index = 0;
+            // 查找在 nums2 中的位置
+            while (index < nums2.length && nums1[i] != nums2[index]) index++;
+            if (index == nums2.length) result[i] = -1;
+            // 找下一个更大元素的位置
+            while (index < nums2.length && nums1[i] >= nums2[index]) index++;
+            result[i] = (index == nums2.length) ? -1 : nums2[index];
+        }
+        return result;
+    }
+}
