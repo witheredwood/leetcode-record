4839b42a89bce244a9a62222cfe42ecbdd377b83
witheredwood <witheredwood7@163.com>
2022-07-27 21:38:14 +0800

    104

--- src/code/S07_tree/code_0104.java
+++ src/code/S07_tree/code_0104.java
@@ -1,14 +1,13 @@
 package code.S07_tree;
 
 import java.util.LinkedList;
-import java.util.List;
 
 /**
  * 104.二叉树的最大深度
  * <p>
  * 给定一个二叉树，找出其最大深度。
  * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
- * 说明: 叶子节点是指没有子节点的节点。
+ * 说明:叶子节点是指没有子节点的节点。
  * <p>
  * 示例：
  * 给定二叉树 [3,9,20,null,null,15,7]，
@@ -17,13 +16,24 @@ import java.util.List;
  * 9  20
  * /  \
  * 15   7
- * 返回它的最大深度 3 。
+ * 返回它的最大深度3 。
  * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class code_0104 {
+    /**
+     * 后序遍历
+     *
+     * @param root 二叉树根节点
+     * @return 二叉树的最大深度
+     */
+    public int maxDepth31(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth31(root.left), maxDepth31(root.right)) + 1;
+    }
+
     /**
      * 递归解法
      *
