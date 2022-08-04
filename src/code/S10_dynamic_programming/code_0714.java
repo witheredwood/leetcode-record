6b6552230ecd621207e20c48bc8d29c93154f88e
witheredwood <witheredwood7@163.com>
2022-08-04 15:59:41 +0800

    714

--- src/code/S10_dynamic_programming/code_0714.java
+++ src/code/S10_dynamic_programming/code_0714.java
@@ -3,7 +3,7 @@ package code.S10_dynamic_programming;
 /**
  * 714. 买卖股票的最佳时机含手续费
  * <p>
- * 给定一个整数数组 prices，其中第 i 个元素代表了第 i 天的股票价格 ；整数 fee 代表了交易股票的手续费用。
+ * 给定一个整数数组 prices，其中第i个元素代表了第i天的股票价格 ；整数 fee 代表了交易股票的手续费用。
  * 你可以无限次地完成交易，但是你每笔交易都需要付手续费。如果你已经购买了一个股票，在卖出它之前你就不能再继续购买股票了。
  * 返回获得利润的最大值。
  * <p>
@@ -13,25 +13,50 @@ package code.S10_dynamic_programming;
  * 输入：prices = [1, 3, 2, 8, 4, 9], fee = 2
  * 输出：8
  * 解释：能够达到的最大利润:
- * 在此处买入 prices[0] = 1
+ * 在此处买入prices[0] = 1
  * 在此处卖出 prices[3] = 8
  * 在此处买入 prices[4] = 4
  * 在此处卖出 prices[5] = 9
- * 总利润: ((8 - 1) - 2) + ((9 - 4) - 2) = 8
+ * 总利润:((8 - 1) - 2) + ((9 - 4) - 2) = 8
  * 示例 2：
  * 输入：prices = [1,3,7,5,10,3], fee = 3
  * 输出：6
  * <p>
  * 提示：
- * 1 <= prices.length <= 5 * 104
- * 1 <= prices[i] < 5 * 104
- * 0 <= fee < 5 * 104
+ * 1 <= prices.length <= 5 * 10^4
+ * 1 <= prices[i] < 5 * 10^4
+ * 0 <= fee < 5 * 10^4
  * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class code_0714 {
+
+    /**
+     * time: O(n);      space: O(1)
+     * <p>
+     * 买入时加上手续费
+     * 卖出股票并且获得 prices[i] - buy 的收益。
+     * 卖出时可以看成，当前手上拥有一支买入价格为  prices[i] 的股票，以提供一个反悔操作
+     *
+     * @param prices 某段时间内的股票价格
+     * @param fee    每次交易的手续费
+     * @return 该段时间内的最大利润
+     */
+    public int maxProfit31(int[] prices, int fee) {
+        int buy = prices[0] + fee, profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] + fee < buy) {    // 重新买入
+                buy = prices[i] + fee;
+            } else if (prices[i] > buy) {   // 卖出
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+
     /**
      * 贪心。目标：如何获得最大收益
      * 当我们卖出一支股票时，我们就立即获得了以相同价格并且免除手续费买入一支股票的权利
