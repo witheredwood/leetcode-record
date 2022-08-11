f059e1e98b7596e4a84ff3bed9920794046754e3
witheredwood <witheredwood7@163.com>
2022-08-11 11:33:24 +0800

    78

--- src/code/S08_backtracking/code_0078.java
+++ src/code/S08_backtracking/code_0078.java
@@ -1,12 +1,14 @@
 package code.S08_backtracking;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
  * 78.子集
  * <p>
- * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ * 给你一个整数数组nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
  * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
  * <p>
  * 示例 1：
@@ -15,7 +17,7 @@ import java.util.List;
  * 示例 2：
  * 输入：nums = [0]
  * 输出：[[],[0]]
- *  
+ * <p>
  * 提示：
  * 1 <= nums.length <= 10
  * -10 <= nums[i] <= 10
@@ -25,6 +27,29 @@ import java.util.List;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class code_0078 {
+
+    /**
+     * @param nums 整数数组，不包含重复的数字
+     * @return 所有的子集
+     */
+    public List<List<Integer>> subsets31(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        List<Integer> path = new LinkedList<>();
+        backtracking(nums, 0, path, result);
+        return result;
+    }
+
+    private void backtracking(int[] nums, int startIndex, List<Integer> path, List<List<Integer>> result) {
+        if (startIndex > nums.length) return;
+        result.add(new ArrayList<>(path));
+
+        for (int i = startIndex; i < nums.length; i++) {
+            path.add(nums[i]);
+            backtracking(nums, i + 1, path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+
     /**
      * 回溯
      *
